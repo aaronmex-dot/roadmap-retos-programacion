21260d4265d05b91808893f3f2c06f37ccb8d4da
aaronmex-dot <aaron.mex230988@gmail.com>
2026-09-22 10:18:31 +0100

    Create aaronmex-dot.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/aaronmex-dot.py
@@ -0,0 +1,25 @@
+ 
+
+# print("https://www.python.org/")
+
+# Aqui se hace un comentario de una linea
+"""
+Se usan las treas lineas para poder hacer un comentario mas amplio en muchas lineas
+Esto es un comentario tambien
+"""
+
+```
+Esto tambien es otra
+forma de escribir comentarios
+```
+my_variable = "Benjamin" 
+MY_CONSTANT = "Mi constante"   # por convencion 
+string = "Nombre"
+integer = 3 
+float = 3.1416
+booleano = True
+booleano1 = False 
+
+print(" Hola, estoy aprendiendo python")
+
+
